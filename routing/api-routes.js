f7f5dfc26b9e743321a11472832d72d80533788b
Videl90 <lorena.sa.0117@gmail.com>
2020-08-18 23:31:45 -0500

    added some finall styles

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -37,29 +37,5 @@ module.exports = function (app) {
         dbNotes.splice(allDbNotes, 1);
 
         res.send(deleteNote);
-        /*let idNote = req.params.id;
-        console.log("idNote", idNote);
-        
-        fs.readFile('./db/db.json', parsedata, (err, data) => {
-            const newDbNotes = dbNotes.filter(note => note.id != idNote);
-            console.log("newDbNotes:", newDbNotes)
-            let parsedata = JSON.stringify(newDbNotes);         
-            fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
-                if (err) throw err;
-            })
-            res.json(newDbNotes);
-            console.log("note deleted")
-        })
-        for (var i = 0; i < dbNotes.length; i++){
-            if (idNote === dbNotes[i].idNote){
-                dbNotes = dbNotes.filter((note) => {
-                    return note.id != idNote;
-                });
-                fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
-                    if (err) throw err;
-                })
-                res.json(dbNotes);
-            }
-        }*/
     })
 }

eefb7e5df45a5d1e5ebba5e9545c86da9aaad764
Videl90 <lorena.sa.0117@gmail.com>
2020-08-18 22:58:13 -0500

    finally working

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -1,5 +1,6 @@
 var dbNotes = require('../db/db.json');
-var fs = require ("fs");
+var fs = require('fs');
+var path = require('path');
 console.log(dbNotes);
 
 module.exports = function (app) {
@@ -14,13 +15,51 @@ module.exports = function (app) {
         let noteNum = dbNotes.length + 1;
         newNote.id = noteNum;
 
-        fs.readFile(dbNotes, (err, data) => {
-            db.push(newNote);
-            res.send(dbNotes);
+        dbNotes.push(newNote)
+
+        let parsedata = JSON.stringify(dbNotes);         
+        fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
+            if (err) throw err;
         })
+        res.json(dbNotes);
+
     });
 
-    app.delete('/api/notes/:id', function(req, data){
+    app.delete('/api/notes/:id', (req, res) => {
+
+        const deleteNote = dbNotes.find(note => note.id === parseInt(req.params.id))
+        console.log("delete note", deleteNote)
         
+        if(!deleteNote)
+        return res.send("Note deleted");
+
+        const allDbNotes = dbNotes.indexOf();
+        dbNotes.splice(allDbNotes, 1);
+
+        res.send(deleteNote);
+        /*let idNote = req.params.id;
+        console.log("idNote", idNote);
+        
+        fs.readFile('./db/db.json', parsedata, (err, data) => {
+            const newDbNotes = dbNotes.filter(note => note.id != idNote);
+            console.log("newDbNotes:", newDbNotes)
+            let parsedata = JSON.stringify(newDbNotes);         
+            fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
+                if (err) throw err;
+            })
+            res.json(newDbNotes);
+            console.log("note deleted")
+        })
+        for (var i = 0; i < dbNotes.length; i++){
+            if (idNote === dbNotes[i].idNote){
+                dbNotes = dbNotes.filter((note) => {
+                    return note.id != idNote;
+                });
+                fs.writeFile(path.join('./db/db.json'), parsedata, (err) => {
+                    if (err) throw err;
+                })
+                res.json(dbNotes);
+            }
+        }*/
     })
 }
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser'); express has a 
 var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var apiRoutes = require('./routing/api-routes.js');
-var htmlRoutes = require('./routing/html-routes.js');
+//var path = require('path');  we are not using this one here
+//var fs = require('fs'); we are not using this one here
+//var apiRoutes = require('./routing/api-routes.js');
+//var htmlRoutes = require('./routing/html-routes.js');
 
 //CHOSEN PORT
 var app = express();
@@ -11,20 +11,20 @@ var PORT = process.env.PORT || 8080;
 
 
 //BODYPARSER
-app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false })) // this must be from express build in urlencoded
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()) // I changed bodyParser for express...
+//app.use(express.static(path.join(__dirname, '../public/css/styles.css')));
+// we just need to pass the static folder here 
+app.use(express.static("public"));
 
-app.use(bodyParser.json())
-
-app.use(express.static(path.join(__dirname, '../public/css/styles.css')));
-
-app.use("/api", apiRoutes);
-
-app.use("/", htmlRoutes);
+// ... yeah we don't need this, is just another way to do it
+//app.use("/api", apiRoutes); 
+//app.use("/", htmlRoutes);
 
 //ROUTES
-//require('./routing/api-routes.js')(app);
-//require('./routing/html-routes.js')(app);
-
+require('./routing/api-routes.js')(app);
+require('./routing/html-routes.js')(app);
 
 //SERVER LISTEN
 app.listen(PORT, function(){
